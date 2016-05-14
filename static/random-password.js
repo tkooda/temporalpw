8d4aacc560353f905455b811413191ee96646c7b
Thor J. Kooda <tkooda@github>
2016-05-14 16:24:00 -0500

    remove unnessicary external library, cleanup random-password.js, fix typos, update about

--- static/random-password.js
+++ static/random-password.js
@@ -1,16 +1,11 @@
 // attempt to generate cryptographically secure random byte, with optional max for using as a charset index
 function getRandomByte( max = 256 ) {
     // http://caniuse.com/#feat=getrandomvalues
-    if ( window.crypto && window.crypto.getRandomValues ) {
+    var crypto = window.crypto || window.msCrypto;
+    if ( crypto && crypto.getRandomValues ) {
         var a = new Uint8Array( 1 );
         while ( true ) {
-            window.crypto.getRandomValues( a );
-            if ( a[0] <= max ) return a[0];
-        }
-    } else if ( window.msCrypto && window.msCrypto.getRandomValues ) {
-        var a = new Uint8Array( 1 );
-        while ( true ) {
-            window.msCrypto.getRandomValues( a );
+            crypto.getRandomValues( a );
             if ( a[0] <= max ) return a[0];
         }
     } else {
@@ -20,8 +15,8 @@ function getRandomByte( max = 256 ) {
 
 function generatePassword( minLength = 20, maxLength = 30, charset = "abcdefghijknopqrstuvwxyzACDEFGHJKLMNPQRSTUVWXYZ2345679" ) {
     if ( minLength > maxLength) maxLength = minLength;
-    var randomLength = Math.floor( Math.random() * ( maxLength - minLength ) ) + minLength,
-        password = "";
+    var randomLength = Math.floor( Math.random() * ( maxLength - minLength ) ) + minLength;
+    var password = "";
     for ( var i = 0, maxIndex = charset.length; i < randomLength; i++ ) {
         password += charset.charAt( getRandomByte( maxIndex ) );
     }
